35f867e51909f2bff6d2478bcc0ef9d29c3df448
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2023-01-21 23:42:31 +0330

    feat: insert user into database, hashed password

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,PrismaService]
 })
 export class UserModule {}
