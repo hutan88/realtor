df8bab0397e00e7c2daceb17588ffd216af87541
Hutan Alinaghi <hutan.alinaghi88@gmail.com>
2023-01-29 00:54:02 +0330

    feat: define the response dto

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -5,12 +5,36 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
-
   async getAll(): Promise<HomeDto[]> {
-    const getAllHome = await this.prismaService.home.findMany();
-    return getAllHome.map((home) => new HomeDto(home));
-  }
-
+    const  getAllHome =  await this.prismaService.home.findMany(
+      {
+        select:{
+          id: true,
+          address: true,
+          city: true,
+          land_size: true,
+          number_of_bathrooms: true,
+          number_of_bedrooms: true,
+          listed_date: true,
+          price: true,
+          property_type: true,
+          images:{
+            select:{
+              id: true,
+              url: true
+            },
+            take: 1
+          }
+        }
+      }
+    );
+  return getAllHome.map(home=> 
+    {
+      const fetcHome = {...home,image: home.images[0].url}
+      delete fetcHome.images;
+       return new HomeDto(fetcHome)
+    });
+    };
   async getOne() {}
 
   async create() {}
